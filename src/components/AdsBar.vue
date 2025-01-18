59ced92c9803bee3eb60b2e99ff8a5b9317a8e44
Mohamed YOUSFI <mr.mohamed.yousfi@gmail.com>
2025-01-18 02:27:02 +0100

    Initialisation du projet avec les packages && Intégration graphique du header

---
+++ src/components/AdsBar.vue
@@ -0,0 +1,20 @@
+<script setup>
+import { SfIconCheckCircle, SfIconClose } from '@storefront-ui/vue';
+</script>  
+
+<template>
+    <div
+        role="alert"
+        class="flex items-start md:items-center shadow-md bg-positive-100 pr-2 pl-4 ring-1 ring-positive-200 typography-text-sm md:typography-text-base py-1 rounded-md"
+    >
+        <p class="py-2 mr-2 w-full text-center">Get Up to <strong>40% OFF</strong> New-Season Styles <strong class="bg-primary-700 text-primary-50 py-1 px-2 text-sm">Men</strong> <strong class="bg-primary-700 text-primary-50 py-1 px-2 text-sm">Women</strong> <span class="text-slate-300">*Limited time only</span></p>
+        <button
+        type="button"
+        class="p-1.5 md:p-2 ml-auto rounded-md text-positive-700 hover:bg-positive-200 active:bg-positive-300 hover:text-positive-800 active:text-positive-900 focus-visible:outline focus-visible:outline-offset"
+        aria-label="Close positive alert"
+        >
+        <SfIconClose class="hidden md:block" />
+        <SfIconClose size="sm" class="block md:hidden" />
+        </button>
+    </div>
+</template>
---
+++ src/components/NavMenuTop.vue
@@ -0,0 +1,49 @@
+<template>
+    <nav class="flex justify-center w-full py-2 px-4 lg:py-2 lg:px-6 bg-white border-b border-neutral-200">
+        <div class="flex flex-wrap justify-between lg:flex-nowrap items-center flex-row md:justify-start h-full max-w-[1536px] w-full">
+          <p class="mr-auto uppercase text-sm text-gray-600 font-bold">Free Returns. Standard Shipping Orders $99+</p>
+            <div class="flex flex-wrap lg:flex-nowrap whitespace-nowrap">
+            <SfButton
+                v-for="item in items"
+                :key="item.label"
+                variant="tertiary"
+                :class="[
+                'py-1 flex h-full w-full text-gray-600 text-sm',
+                ]"
+            >
+                {{ item.label }}
+            </SfButton>
+        </div>
+        </div>
+    </nav>
+  </template>
+  <script setup>
+  import { SfButton } from '@storefront-ui/vue';
+
+  const items = [
+    {
+      label: 'My Account',
+    },
+    {
+      label: 'About US',
+    },
+    {
+      label: 'Blog',
+    },
+    {
+      label: 'My Watchlist',
+    },
+    {
+      label: 'Cart',
+    },
+    {
+      label: 'Log In',
+    },
+    {
+      label: 'Eng',
+    },
+    {
+      label: 'USD',
+    },
+  ];
+  </script>  
